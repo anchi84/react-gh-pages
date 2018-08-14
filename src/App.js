8353282268492b6e04e492119cc22fbfb4e08cb4
unknown <anarili@gmail.com>
2018-08-14 03:19:22 +0200

    products and product added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import Products from './components/products';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        products:[]
+    };
+  }
+
+  componentDidMount() {
+    const url = `https://my-json-server.typicode.com/anchi84/react-gh-pages/products`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.setState({ products: data });
+        });
+}
   render() {
     return (
       <div>
         <Header/>
+        <Products products={this.state.products}/>
         <Footer/>
       </div>
     );
